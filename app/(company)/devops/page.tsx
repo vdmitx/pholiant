35bcc230276cf3fe5c03d6a66ca6b5207bd6f159
vdmitx <129660122+vdmitx@users.noreply.github.com>
2023-11-30 14:36:25 +0300

    add info DevOps

--- app/(company)/devops/page.tsx
+++ app/(company)/devops/page.tsx
@@ -1,11 +1,48 @@
 import { delay } from '@/lib/utils'
+import React from 'react';
+import { Divider } from 'antd';
 
 export default async function Page() {
   await delay(1000)
 
   return (
     <div>
-      <h1 className='text-3xl font-bold'>DevOps</h1>
+        <h1 className='text-3xl font-bold'>DevOps</h1>
+        <Divider />
+        <p>YAML</p>
+        <p>Example</p>
+        <pre>{`
+            #YAML
+            Imaro:
+            author: Charles R. Saunders
+            language: English
+            publication-year: 1981
+            pages: 224
+        `}</pre>
+        <Divider />
+        <p>JSON</p>
+        <p>Example</p>
+        <pre>{`
+           {
+                "Imaro": {
+                "author": "Charles R. Saunders",
+                "language": "English",
+                "publication-year": "1981",
+                "pages": 224
+                }
+            }
+        `}</pre>
+        <Divider dashed />
+        <h3>What?</h3>
+        <p>Example</p>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere mihi ista
+            probare, quae sunt a te dicta? Refert tamen, quo modo.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere mihi ista
+            probare, quae sunt a te dicta? Refert tamen, quo modo.
+        </p>
     </div>
+    
   )
 }
+
--- app/(company)/team/page.tsx
+++ app/(company)/team/page.tsx
@@ -2,7 +2,6 @@ import { delay } from '@/lib/utils'
 
 export default async function Page() {
   await delay(1000)
-
   return (
     <div>
       <h1 className='text-3xl font-bold'>Team</h1>
