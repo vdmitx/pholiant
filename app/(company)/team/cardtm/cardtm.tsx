eb07fca35198767f9072e50bfa3bbac4beb3611c
vdmitx <129660122+vdmitx@users.noreply.github.com>
2023-12-06 15:19:25 +0300

    Card componet upd

--- app/(company)/team/cardtm/cardtm.tsx
+++ app/(company)/team/cardtm/cardtm.tsx
@@ -8,25 +8,29 @@ import logo from '../../logo.png';
 
 export default function ActionAreaCard() {
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          height="140"
-          image="../../logo.png"
-          alt="green iguana"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Lizard
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
+    <div className='flex'>
+      <Card sx={{ maxWidth: 345 }} >
+        {itemData.map((item) => (
+          <CardActionArea key={item.id} >
+          <CardMedia
+            component="img"
+            height="140"
+            image={`${item.img}?w=248&fit=crop&auto=format`}
+            alt={item.title}
+            loading="lazy"
+            />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+            {item.title}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+            {item.title}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        ))};
+      </Card>
+    </div>
   );
 }
 
@@ -64,65 +68,78 @@ export default function ActionAreaCard() {
 //   );
 // }
 
-// const itemData = [
-//   {
-//     img: "https://images.unsplash.com/photo-1549388604-817d15aa0110",
-//     title: "Bed",
-//     author: "swabdesign",
-//   },
-//   {
-//     img: "https://images.unsplash.com/photo-1525097487452-6278ff080c31",
-//     title: "Books",
-//     author: "Pavel Nekoranec",
-//   },
-//   {
-//     img: "https://images.unsplash.com/photo-1523413651479-597eb2da0ad6",
-//     title: "Sink",
-//     author: "Charles Deluvio",
-//   },
-//   {
-//     img: "https://images.unsplash.com/photo-1563298723-dcfebaa392e3",
-//     title: "Kitchen",
-//     author: "Christian Mackie",
-//   },
-//   {
-//     img: "https://images.unsplash.com/photo-1588436706487-9d55d73a39e3",
-//     title: "Blinds",
-//     author: "Darren Richardson",
-//   },
-//   {
-//     img: "https://images.unsplash.com/photo-1574180045827-681f8a1a9622",
-//     title: "Chairs",
-//     author: "Taylor Simpson",
-//   },
-//   {
-//     img: "https://images.unsplash.com/photo-1530731141654-5993c3016c77",
-//     title: "Laptop",
-//     author: "Ben Kolde",
-//   },
-//   {
-//     img: "https://images.unsplash.com/photo-1481277542470-605612bd2d61",
-//     title: "Doors",
-//     author: "Philipp Berndt",
-//   },
-//   {
-//     img: "https://images.unsplash.com/photo-1517487881594-2787fef5ebf7",
-//     title: "Coffee",
-//     author: "Jen P.",
-//   },
-//   {
-//     img: "https://images.unsplash.com/photo-1516455207990-7a41ce80f7ee",
-//     title: "Storage",
-//     author: "Douglas Sheppard",
-//   },
-//   {
-//     img: "https://images.unsplash.com/photo-1597262975002-c5c3b14bbd62",
-//     title: "Candle",
-//     author: "Fi Bell",
-//   },
-//   {
-//     img: "https://images.unsplash.com/photo-1519710164239-da123dc03ef4",
-//     title: "Coffee table",
-//     author: "Hutomo Abrianto",
-//   },
-// ];
+const itemData = [
+  {
+    id: 1,
+    img: "https://images.unsplash.com/photo-1549388604-817d15aa0110",
+    title: "Bed",
+    author: "swabdesign",
+    description: "lorem10"
+  },
+  {
+    id: 2,
+    img: "https://images.unsplash.com/photo-1525097487452-6278ff080c31",
+    title: "Books",
+    author: "Pavel Nekoranec",
+  },
+  {
+    id: 3,
+    img: "https://images.unsplash.com/photo-1523413651479-597eb2da0ad6",
+    title: "Sink",
+    author: "Charles Deluvio",
+  },
+  {
+    id: 4,
+    img: "https://images.unsplash.com/photo-1563298723-dcfebaa392e3",
+    title: "Kitchen",
+    author: "Christian Mackie",
+  },
+  {
+    id: 5,
+    img: "https://images.unsplash.com/photo-1588436706487-9d55d73a39e3",
+    title: "Blinds",
+    author: "Darren Richardson",
+  },
+  {
+    id: 6,
+    img: "https://images.unsplash.com/photo-1574180045827-681f8a1a9622",
+    title: "Chairs",
+    author: "Taylor Simpson",
+  },
+  {
+    id: 7,
+    img: "https://images.unsplash.com/photo-1530731141654-5993c3016c77",
+    title: "Laptop",
+    author: "Ben Kolde",
+  },
+  {
+    id: 8,
+    img: "https://images.unsplash.com/photo-1481277542470-605612bd2d61",
+    title: "Doors",
+    author: "Philipp Berndt",
+  },
+  {
+    id: 9,
+    img: "https://images.unsplash.com/photo-1517487881594-2787fef5ebf7",
+    title: "Coffee",
+    author: "Jen P.",
+  },
+  {
+    id: 10,
+    img: "https://images.unsplash.com/photo-1516455207990-7a41ce80f7ee",
+    title: "Storage",
+    author: "Douglas Sheppard",
+  },
+  {
+    id: 11,
+    img: "https://images.unsplash.com/photo-1597262975002-c5c3b14bbd62",
+    title: "Candle",
+    author: "Fi Bell",
+  },
+  {
+    id: 12,
+    img: "https://images.unsplash.com/photo-1519710164239-da123dc03ef4",
+    title: "Coffee table",
+    author: "Hutomo Abrianto",
+  },
+];
